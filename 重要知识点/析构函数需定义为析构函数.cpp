10ff1fd765cc18fbde61ed3cbe539461278afdec
yifan <1278841724@qq.com>
2021-04-21 16:21:37 +0800

    父类析构函数应该定义为虚函数

---
+++ 重要知识点/析构函数需定义为析构函数.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Person
+{
+public:
+    Person() { cout << "Person constructor" << endl; };
+    // ~Person() { cout << "Person deconstructor" << endl; }; //析构函数不定义为虚函数
+    virtual ~Person() { cout << "Person deconstructor" << endl; };
+};
+
+class Stu : public Person
+{
+public:
+    Stu() { cout << "Stu constructor" << endl; };
+    ~Stu() { cout << "Stu deconstructor" << endl; };
+};
+
+int main()
+{
+    Person *p = new Stu();
+    delete p; //释放
+    return 0;
+}
